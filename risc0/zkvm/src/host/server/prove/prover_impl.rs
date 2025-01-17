036e66c43a27d9783ff29e8951a513fc254fdee1
Erik Kaneda <erik@risczero.com>
2025-01-17 14:25:12 -0800

    wip: add mmr on the receipt side. TODO: refactor mmr to single, parameterized implementation

--- risc0/zkvm/src/host/server/prove/prover_impl.rs
+++ risc0/zkvm/src/host/server/prove/prover_impl.rs
@@ -23,14 +23,13 @@ use crate::{
         client::prove::ReceiptKind,
         prove_info::ProveInfo,
         recursion::{identity_p254, join, lift, resolve},
-        server::session::InnerSegment,
+        server::{prove::union_mmr::UnionMmr, session::InnerSegment},
     },
     prove_registered_zkr,
     receipt::{
         segment::decode_receipt_claim_from_seal, InnerReceipt, SegmentReceipt, SuccinctReceipt,
     },
     receipt_claim::{MaybePruned, Merge, Unknown},
-    recursion::prove::union,
     sha::Digestible,
     Assumption, AssumptionReceipt, CompositeReceipt, InnerAssumptionReceipt, Output, ProverOpts,
     Receipt, ReceiptClaim, Segment, Session, VerifierContext,
@@ -111,6 +110,7 @@ impl ProverServer for ProverImpl {
             zkr_receipts.insert(assumption, receipt);
         }
 
+        let mut keccak_receipts: UnionMmr = UnionMmr::default();
         for proof_request in session.pending_keccaks.iter() {
             let receipt = prove_keccak(proof_request)?;
             let assumption = Assumption {
@@ -119,8 +119,17 @@ impl ProverServer for ProverImpl {
             };
             tracing::debug!("adding keccak assumption: {assumption:#?}");
             zkr_receipts.insert(assumption, receipt.clone());
+            keccak_receipts.insert(receipt);
         }
-        // TODO: call union here
+
+        let root_receipt: SuccinctReceipt<Unknown> = keccak_receipts.root()?;
+        zkr_receipts.insert(
+            Assumption {
+                claim: root_receipt.claim.digest(),
+                control_root: root_receipt.control_root()?,
+            },
+            root_receipt,
+        );
 
         // TODO: add test case for when a single session refers to the same assumption multiple times
         let inner_assumption_receipts: Vec<_> = session_assumption_receipts
